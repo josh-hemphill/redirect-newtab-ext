cfad6b178af1bcca950bf6516b2d4215334129d7
josh-hemphill <j1.robofan@gmail.com>
2025-10-26 00:14:38 -0700

    ci: fix zip file creation based on OS

--- scripts/create-artifact.ts
+++ scripts/create-artifact.ts
@@ -1,10 +1,18 @@
-import { $, fs } from 'zx';
+import { $, fs, os, usePowerShell } from 'zx';
 import * as PackageJson from '../package.json' with { type: 'json' };
 
+const viteExists = await fs.exists('./dist/.vite');
 await Promise.all([
 	fs.copyFile('./LICENSE', './dist/LICENSE'),
 	fs.copyFile('./README.md', './dist/README.md'),
-	fs.rm('./dist/.vite', { recursive: true }),
+	viteExists ? fs.rm('./dist/.vite', { recursive: true }) : Promise.resolve(),
 ]);
 
-await $`tar -acf newtab-redirect-${PackageJson.version}.zip dist`;
+const hasZip = await $`which zip`.nothrow().quiet();
+if (hasZip.exitCode !== 0 && os.type().toLowerCase().includes('windows')) {
+	usePowerShell();
+	await $`Compress-Archive -Force -Path dist\\* -DestinationPath newtab-redirect-${PackageJson.version}.zip`;
+}
+else {
+	await $`zip -r newtab-redirect-${PackageJson.version}.zip dist/*`;
+}
